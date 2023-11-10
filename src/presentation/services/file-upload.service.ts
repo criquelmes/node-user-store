8d80be21300aaa1850fabd32cc024fddfa057a07
Christopher Riquelme <criquelmes@live.com>
2023-11-10 19:47:08 -0300

    Sección 22: Clase - Mover archivo a destino permanente

---
+++ src/presentation/services/file-upload.service.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+import { UploadedFile } from "express-fileupload";
+
+export class FileUploadService {
+  constructor() {}
+
+  private checkFolder(folderPath: string) {
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+  }
+
+  public async uploadSingle(
+    file: UploadedFile,
+    folder: string = "uploads",
+    validExtensions: string[] = ["png", "jpg", "jpeg", "gif"]
+  ) {
+    try {
+      const fileExtension = file.mimetype.split("/").at(1);
+      const destination = path.resolve(__dirname, "../../../", folder);
+      this.checkFolder(destination);
+
+      file.mv(destination + `/mi-imagen.${fileExtension}`);
+    } catch (error) {
+      console.log({ error });
+    }
+  }
+
+  public uploadMultiple(
+    file: any[],
+    folder: string = "uploads",
+    validExtensions: string[] = ["png", "jpg", "jpeg", "gif"]
+  ) {}
+}
